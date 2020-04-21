d5b8a37b8d0dd7d64565e4eb8521b78a8d6cd32d
PaiHsuehChung <eddieworkmail0401@gmail.com>
2020-04-21 17:27:27 +0800

    Add docstring, keras, typing function!

---
+++ docstring/main.py
@@ -0,0 +1,12 @@
+def mainFunction(a, b,c=0):
+    '''
+    [summary]
+    
+    :param a: [description]
+    :type a: [type]
+    :param b: [description]
+    :type b: [type]
+    :param c: [description], defaults to 0
+    :type c: int, optional
+    '''
+
---
+++ typing/main.py
@@ -0,0 +1,20 @@
+
+
+def typing(a:int, b:list) -> str :
+    '''
+    [summary]
+    
+    :param a: [description]
+    :type a: int
+    :param b: [description]
+    :type b: list
+    :return: [description]
+    :rtype: str
+    '''
+
+    return a+int(b)
+
+
+
+if __name__ == '__main__':
+   print(typing(10, 5))
